233c026491194c0c5dcdfe190b2df7c4b66eff67
MrTruitedev <tomtutti@hotmail.fr>
2022-05-30 16:58:45 +0200

    30/05/2022-2

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ConferenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_conference')]
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    {
+        return new Response($twig->render('conference/index.html.twig', [
+        'conferences' => $conferenceRepository->findAll(),
+    ]));
+    }
+}
